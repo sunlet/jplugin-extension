0602c819fa4e83c30f4deb483435e863851c7527
cbq881215 <cbq881215@126.com>
2020-07-17 15:44:47 +0800

    配置

--- jplugin-embed-tomcat/src/main/java/net/jplugin/extension/embed_tomcat/impl/EmbedTomcatConfig.java
+++ jplugin-embed-tomcat/src/main/java/net/jplugin/extension/embed_tomcat/impl/EmbedTomcatConfig.java
@@ -5,47 +5,142 @@ import net.jplugin.core.config.api.ConfigFactory;
 import net.jplugin.core.kernel.api.PluginEnvirement;
 
 public class EmbedTomcatConfig {
-	//是否支持Web
-	private static final String USE_WEB_SUPPORT = "embed-tomcat.use-web-support";
-	//web端口号
-	private static final String TOMCAT_PORT="embed-tomcat.context-port";
-	
-	//context name。 http://aaaaa:8081/[context name]/aaa/bbb.do
-	//默认为""
-	private static final String CONTEXT_NAME="embed-tomcat.context-name";
-	
-	private static Integer tomcatPort;
-	private static String contextName;
-	private static boolean useWebSupport;
-	
-	public static void init() {
-		tomcatPort = ConfigFactory.getIntConfig(TOMCAT_PORT, 8080);
-		
-		useWebSupport = !"false".equalsIgnoreCase(ConfigFactory.getStringConfigWithTrim(USE_WEB_SUPPORT));
-		
-		contextName = ConfigFactory.getStringConfigWithTrim(CONTEXT_NAME);
-		if (StringKit.isNull(contextName)) contextName = "";
-		
-		StringBuffer sb = new StringBuffer();
-		sb.append("$$$ Embed Tomcat config:\n tomcatPort="+tomcatPort)
-		.append("\n useWebSupport:"+useWebSupport)
-		.append("\n contextName:"+contextName);
-		
-		PluginEnvirement.getInstance().getStartLogger().log(sb.toString());
-	}
-	
-	public static Integer getTomcatPort() {
-		return tomcatPort;
-	}
-	
-//	public static String getWebAppDir() {
+    //是否支持Web
+    private static final String USE_WEB_SUPPORT = "embed-tomcat.use-web-support";
+    //web端口号
+    private static final String TOMCAT_PORT = "embed-tomcat.context-port";
+
+    //context name。 http://aaaaa:8081/[context name]/aaa/bbb.do
+    //默认为""
+    private static final String CONTEXT_NAME = "embed-tomcat.context-name";
+
+
+    private static final String PROTOCOL_COMPRESSABLE_MIME_TYPE = "embed-tomcat.protocol.compressable-mime-type";
+
+
+    private static final String PROTOCOL_MAX_THREADS = "embed-tomcat.protocol.max-threads";
+
+
+    private static final String PROTOCOL_MAX_CONNECTIONS = "embed-tomcat.protocol.max-connections";
+
+    private static final String PROTOCOL_CONNECTION_TIMEOUT = "embed-tomcat.protocol.connection-timeout";
+
+    private static final String PROTOCOL_MIN_SPARE_THREADS = "embed-tomcat.protocol.min-spare-threads";
+
+    private static final String PROTOCOL_KEEPALIVE_TIMEOUT = "embed-tomcat.protocol.keepalive-timeout";
+
+
+    private static final String PROTOCOL_ACCEPTOR_THREADCOUNT = "embed-tomcat.protocol.acceptor-threadCount";
+
+    private static final String REDIRECT_PORT = "embed-tomcat.redirect-port";
+
+    private static final String PROTOCOL_URL_ENCODING = "embed-tomcat.uri-encoding";
+
+    private static final String PROTOCOL_MAX_POST_SIZE = "embed-tomcat.max-post-size";
+
+
+    private static Integer tomcatPort;
+    private static String contextName;
+    private static boolean useWebSupport;
+
+    private static String uriEncoding;
+    private static Integer maxThreads;
+    private static Integer maxConnections;
+    private static Integer connectionTimeout;
+    private static Integer minSpareThreads;
+    private static Integer keepaliveTimeout;
+    private static Integer acceptorThreadCount;
+    private static Integer redirectPort;
+    private static String compressableMimeType;
+    private static Integer maxPostSize;
+
+    public static void init() {
+        tomcatPort = ConfigFactory.getIntConfig(TOMCAT_PORT, 8080);
+
+        useWebSupport = !"false".equalsIgnoreCase(ConfigFactory.getStringConfigWithTrim(USE_WEB_SUPPORT));
+
+        contextName = ConfigFactory.getStringConfigWithTrim(CONTEXT_NAME);
+        if (StringKit.isNull(contextName)) contextName = "";
+
+        uriEncoding = ConfigFactory.getStringConfig(PROTOCOL_URL_ENCODING);
+        maxThreads = ConfigFactory.getIntConfig(PROTOCOL_MAX_THREADS);
+
+        maxConnections = ConfigFactory.getIntConfig(PROTOCOL_MAX_CONNECTIONS);
+        connectionTimeout = ConfigFactory.getIntConfig(PROTOCOL_CONNECTION_TIMEOUT);
+        minSpareThreads = ConfigFactory.getIntConfig(PROTOCOL_MIN_SPARE_THREADS);
+        keepaliveTimeout = ConfigFactory.getIntConfig(PROTOCOL_KEEPALIVE_TIMEOUT);
+        acceptorThreadCount = ConfigFactory.getIntConfig(PROTOCOL_ACCEPTOR_THREADCOUNT);
+
+        redirectPort = ConfigFactory.getIntConfig(REDIRECT_PORT);
+
+        compressableMimeType = ConfigFactory.getStringConfigWithTrim(PROTOCOL_COMPRESSABLE_MIME_TYPE);
+
+        maxPostSize = ConfigFactory.getIntConfig(PROTOCOL_MAX_POST_SIZE);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("$$$ Embed Tomcat config:\n tomcatPort=" + tomcatPort)
+                .append("\n useWebSupport:" + useWebSupport)
+                .append("\n contextName:" + contextName)
+				.append("\n maxThreads:" + maxThreads)
+				.append("\n maxConnections:" + maxConnections)
+				.append("\n connectionTimeout:" + connectionTimeout);
+
+
+        PluginEnvirement.getInstance().getStartLogger().log(sb.toString());
+    }
+
+    public static Integer getTomcatPort() {
+        return tomcatPort;
+    }
+
+    //	public static String getWebAppDir() {
 //		return webAppDir;
 //	}
-	public static boolean isUseWebSupport() {
-		return useWebSupport;
-	}
-	
-	public static String getContextName() {
-		return contextName;
+    public static boolean isUseWebSupport() {
+        return useWebSupport;
+    }
+
+    public static String getContextName() {
+        return contextName;
+    }
+
+    public static Integer getMaxThreads() {
+        return maxThreads;
+    }
+
+    public static Integer getMaxConnections() {
+        return maxConnections;
+    }
+
+    public static Integer getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public static Integer getMinSpareThreads() {
+        return minSpareThreads;
+    }
+
+    public static Integer getKeepaliveTimeout() {
+        return keepaliveTimeout;
+    }
+
+    public static Integer getAcceptorThreadCount() {
+        return acceptorThreadCount;
+    }
+
+    public static Integer getRedirectPort() {
+        return redirectPort;
+    }
+
+    public static String getCompressableMimeType() {
+        return compressableMimeType;
+    }
+
+    public static String getUriEncoding() {
+        return uriEncoding;
+    }
+
+	public static Integer getMaxPostSize() {
+		return maxPostSize;
 	}
 }
--- jplugin-embed-tomcat/src/main/java/net/jplugin/extension/embed_tomcat/impl/TomcatStarter.java
+++ jplugin-embed-tomcat/src/main/java/net/jplugin/extension/embed_tomcat/impl/TomcatStarter.java
@@ -1,7 +1,8 @@
 package net.jplugin.extension.embed_tomcat.impl;
 
-import java.io.File;
-
+import net.jplugin.common.kits.XMLKit;
+import net.jplugin.core.kernel.api.PluginEnvirement;
+import net.jplugin.ext.webasic.impl.PluginServlet;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
@@ -9,104 +10,140 @@ import org.apache.coyote.http11.Http11NioProtocol;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
-import net.jplugin.common.kits.XMLKit;
-import net.jplugin.core.kernel.api.PluginEnvirement;
-import net.jplugin.ext.webasic.impl.PluginServlet;
+import java.io.File;
 
 public class TomcatStarter {
 
-	public static Tomcat start() throws Exception {
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(EmbedTomcatConfig.getTomcatPort());
-		
-		//创建Context
-		Context context = null;
-		String ctxName = EmbedTomcatConfig.getContextName();
-		
-		if (!EmbedTomcatConfig.isUseWebSupport()) {
-			context = tomcat.addContext(ctxName,null);
-		}else {
-			//use web support = true
-			String webdir = PluginEnvirement.getInstance().getWebRootPath();
-			checkRootPathValid(webdir);
-			
-			File file = new File(webdir);
-			if (!file.exists() || !file.isDirectory()) {
-				PluginEnvirement.getInstance().getStartLogger().log("$$$ Web dir is not exists, or not directory, JPlugin home: "+PluginEnvirement.getInstance().getWorkDir());
-				context = tomcat.addContext(ctxName,null);
-			}else {
-				PluginEnvirement.getInstance().getStartLogger().log("$$$ Start with static resource and jsp support, Web dir:" + webdir);
-				context = tomcat.addWebapp(ctxName, webdir);
-			}
-		}
-		//创建Servlet
-		tomcat.addServlet(ctxName, "PluginServlet", new PluginServlet4Embed());
-		context.addServletMappingDecoded("*.do", "PluginServlet");
-		
-		Connector connector = tomcat.getConnector();
-		customConnector(connector);
-		
-		tomcat.start();
-		
-		return tomcat;
-	}
-/**
- * 
- * <Connector port="8080" protocol="org.apache.coyote.http11.Http11NioProtocol"
-               connectionTimeout="20000"
-               URIEncoding="UTF-8"
-               useBodyEncodingForURI="false"
-               maxThreads="1000"
-               minSpareThreads="100"
-               enableLookups="false"
-               redirectPort="8443"
-               acceptCount="800"
-               compression="on"
-               compressionMinSize="20480"
-               noCompressionUserAgents="gozilla, traviata"
-               maxPostSize="209715200"
-               compressableMimeType="text/html,text/xml,text/plain,text/javascript,text/css,application/octet-stream" />
- * 
- * 
- * 
- * @param connector
- */
-	private static void customConnector(Connector connector) {
-		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-		// 设置最大线程数
-		protocol.setMaxThreads(50);
-		// 设置最大连接数
-		protocol.setMaxConnections(100);
-		protocol.setAcceptorThreadCount(50);
-		protocol.setMinSpareThreads(50);
-		// 设置超时时间
-		protocol.setConnectionTimeout(5000);
-		protocol.setKeepAliveTimeout(5000);
+    public static Tomcat start() throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(EmbedTomcatConfig.getTomcatPort());
 
-		protocol.setMaxSavePostSize(20480);
-		protocol.setNoCompressionUserAgents("gozilla, traviata");
-	}
+        //创建Context
+        Context context = null;
+        String ctxName = EmbedTomcatConfig.getContextName();
 
-	/**
-	 * 判断webdir下面的web.XML，一定不能包含JPlugin的非嵌入式Servlet,也不能包含 *.do的定义
-	 * @param webdir
-	 * @throws Exception 
-	 */
-	private static void checkRootPathValid(String webdir) throws Exception {
-		String webxml = webdir +"/WEB-INF/web.xml";
-		File file = new File(webxml);
-		String servletName = PluginServlet.class.getName();
-		
-		if (file.exists()) {
-			Document dom = XMLKit.parseFile(webxml);
-			XMLKit.travelNode(dom.getDocumentElement(), (nd)->{
-				if (nd.getNodeType()==Node.TEXT_NODE) {
-					String temp = nd.getTextContent();
-					if (servletName.equals(temp) || "*.do".contentEquals(temp)){
-						throw new RuntimeException("[net.jplugin.ext.webasic.impl.PluginServlet] servlet and [*.do] mapping must removed when useing embed tomcat!");
-					}
-				}
-			} );
+        if (!EmbedTomcatConfig.isUseWebSupport()) {
+            context = tomcat.addContext(ctxName, null);
+        } else {
+            //use web support = true
+            String webdir = PluginEnvirement.getInstance().getWebRootPath();
+            checkRootPathValid(webdir);
+
+            File file = new File(webdir);
+            if (!file.exists() || !file.isDirectory()) {
+                PluginEnvirement.getInstance().getStartLogger().log("$$$ Web dir is not exists, or not directory, JPlugin home: " + PluginEnvirement.getInstance().getWorkDir());
+                context = tomcat.addContext(ctxName, null);
+            } else {
+                PluginEnvirement.getInstance().getStartLogger().log("$$$ Start with static resource and jsp support, Web dir:" + webdir);
+                context = tomcat.addWebapp(ctxName, webdir);
+            }
+        }
+        //创建Servlet
+        tomcat.addServlet(ctxName, "PluginServlet", new PluginServlet4Embed());
+        context.addServletMappingDecoded("*.do", "PluginServlet");
+
+        Connector connector = tomcat.getConnector();
+        customConnector(connector);
+
+        tomcat.start();
+
+        return tomcat;
+    }
+
+    /**
+     * <Connector port="8080" protocol="org.apache.coyote.http11.Http11NioProtocol"
+     * connectionTimeout="20000" ok
+     * URIEncoding="UTF-8"
+     * useBodyEncodingForURI="false"
+     * maxThreads="1000"  ok
+     * minSpareThreads="100" ok
+     * enableLookups="false"
+     * redirectPort="8443"
+     * acceptCount="800"
+     * compression="on"
+     * compressionMinSize="20480"
+     * noCompressionUserAgents="gozilla, traviata"
+     * maxPostSize="209715200"
+     * compressableMimeType="text/html,text/xml,text/plain,text/javascript,text/css,application/octet-stream" />
+     *
+     * @param connector
+     */
+    private static void customConnector(Connector connector) {
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+
+        //当用户用http请求某个资源，而该资源本身又被设置了必须要https方式访问，此时Tomcat会自动重定向到这个redirectPort设置的https端口。
+        if (EmbedTomcatConfig.getRedirectPort() != null) {
+            connector.setRedirectPort(EmbedTomcatConfig.getRedirectPort());
+        }
+
+        if (EmbedTomcatConfig.getUriEncoding() != null && !"".equals(EmbedTomcatConfig.getUriEncoding())) {
+            connector.setURIEncoding(EmbedTomcatConfig.getUriEncoding());
+        }
+
+
+        // 设置最大线程数
+        if (EmbedTomcatConfig.getMaxThreads() != null) {
+            protocol.setMaxThreads(EmbedTomcatConfig.getMaxThreads());
+        }
+        //线程池中，保持活跃的线程的最小数量，默认为10。
+        if (EmbedTomcatConfig.getMinSpareThreads() != null) {
+            protocol.setMinSpareThreads(EmbedTomcatConfig.getMinSpareThreads());
+        }
+
+
+        // 设置超时时间  ,当client与tomcat建立连接之后,在"connectionTimeout"时间之内,仍然没有得到client的请求数据,此时连接将会被断开
+        if (EmbedTomcatConfig.getConnectionTimeout() != null) {
+            protocol.setConnectionTimeout(EmbedTomcatConfig.getConnectionTimeout());
+        }
+
+        //  tomcat允许接收和处理的最大链接数
+        if (EmbedTomcatConfig.getMaxConnections() != null) {
+            protocol.setMaxConnections(EmbedTomcatConfig.getMaxConnections());
+        }
+        //当无实际数据交互时，链接被保持的时间，单位：毫秒
+        if (EmbedTomcatConfig.getKeepaliveTimeout() != null) {
+            protocol.setKeepAliveTimeout(EmbedTomcatConfig.getKeepaliveTimeout());
+        }
+
+        //默认为1，表示用于accept新链接的线程个数，如果在多核CPU架构下，此值可以设置为2，官方不建议设定超过2个的值。
+        if (EmbedTomcatConfig.getAcceptorThreadCount() != null) {
+            protocol.setAcceptorThreadCount(EmbedTomcatConfig.getAcceptorThreadCount());
+        }
+
+        if(EmbedTomcatConfig.getMaxPostSize()!=null) {
+			connector.setMaxPostSize(EmbedTomcatConfig.getMaxPostSize());
 		}
-	}
+
+        if (EmbedTomcatConfig.getCompressableMimeType() != null && !"".equals(EmbedTomcatConfig.getCompressableMimeType())) {
+            protocol.setCompressibleMimeType(EmbedTomcatConfig.getCompressableMimeType());
+        }
+
+        //protocol.setNoCompressionUserAgents("gozilla, traviata");
+
+    }
+
+    /**
+     * 判断webdir下面的web.XML，一定不能包含JPlugin的非嵌入式Servlet,也不能包含 *.do的定义
+     *
+     * @param webdir
+     * @throws Exception
+     */
+    private static void checkRootPathValid(String webdir) throws Exception {
+        String webxml = webdir + "/WEB-INF/web.xml";
+        File file = new File(webxml);
+        String servletName = PluginServlet.class.getName();
+
+        if (file.exists()) {
+            Document dom = XMLKit.parseFile(webxml);
+            XMLKit.travelNode(dom.getDocumentElement(), (nd) -> {
+                if (nd.getNodeType() == Node.TEXT_NODE) {
+                    String temp = nd.getTextContent();
+                    if (servletName.equals(temp) || "*.do".contentEquals(temp)) {
+                        throw new RuntimeException("[net.jplugin.ext.webasic.impl.PluginServlet] servlet and [*.do] mapping must removed when useing embed tomcat!");
+                    }
+                }
+            });
+        }
+    }
 }
