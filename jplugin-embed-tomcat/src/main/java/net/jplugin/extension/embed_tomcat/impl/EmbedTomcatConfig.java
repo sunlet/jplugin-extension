c766b51e6cccbf3d7889c578a56ef3483b065f5b
sunlet <liuhang.163@163.com>
2020-07-28 09:02:13 +0800

    test

--- jplugin-embed-tomcat/src/main/java/net/jplugin/extension/embed_tomcat/impl/EmbedTomcatConfig.java
+++ jplugin-embed-tomcat/src/main/java/net/jplugin/extension/embed_tomcat/impl/EmbedTomcatConfig.java
@@ -97,6 +97,7 @@ public class EmbedTomcatConfig {
     public static Integer getTomcatPort() {
         return tomcatPort;
     }
+    
 
     //	public static String getWebAppDir() {
 //		return webAppDir;
