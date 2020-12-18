bfd7500a2f62a4ef6ca4adac5c826229fa6ae7a0
sunlet <liuhang.163@163.com>
2020-12-18 13:36:28 +0800

    delete unused

--- jplugin-embed-tomcat/src/test/java/test/net/jplugin/extension/embed_tomcat/Main4EmbedTomcat.java
+++
@@ -1,9 +0,0 @@
-package test.net.jplugin.extension.embed_tomcat;
-
-import net.jplugin.core.kernel.PluginApp;
-
-public class Main4EmbedTomcat {
-	public static void main(String[] args) {
-		PluginApp.main(null);
-	}
-}
